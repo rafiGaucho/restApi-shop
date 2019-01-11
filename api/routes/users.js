a7cc94a85eaef9462ef05da07bf1ec072dfde485
rafiGaucho <rafigaucho10r@gmail.com>
2019-01-11 23:05:05 +0530

    user login added & jwt token created

--- api/routes/users.js
+++ api/routes/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router =express.Router();
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt=require('jsonwebtoken')
+
 const User = require('../models/user.js');
 const Product = require('../models/product.js');
 
@@ -51,6 +53,49 @@ router.post('/signup',(req,res,next)=>{
       error:err,
       message:'from first'
 
+    })
+  })
+})
+
+router.post('/login',(req,res,next)=>{
+  User.find({email:req.body.email})
+  .exec()
+  .then(user=>{
+    if (user.length < 1) {
+      return   res.status(401).json({
+        message:'Auth failed'
+      })
+    }
+    bcrypt.compare(req.body.password,user[0].password,(err,result)=>{
+      if (err) {
+        return res.status(401).json({
+          message:'Auth failed'
+        })
+      }
+      if (result) {
+        const token = jwt.sign(
+          {
+            email:user[0].email,
+            userId:user[0]._id
+          },
+          "secret",
+          {
+            expiresIn:"1h"
+          }
+        )
+        return res.status(200).json({
+          message:'Auth succesful',
+          token:token
+        })
+      }
+      res.status(401).json({
+        message:'Auth failed'
+      })
+    })
+  })
+  .catch(err=>{
+    res.status(500).json({
+      error:err
     })
   })
 })
